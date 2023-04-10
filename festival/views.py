d22bf2f18b76db65f108e4f27473072afb268ea5
maksitox <max.poliuha@gmail.com>
2023-04-10 23:50:46 +0300

    First Commit

--- festival/views.py
+++ festival/views.py
@@ -8,11 +8,10 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import Group
 from jinja2 import Environment
 from jinja2.filters import FILTERS
-import openai
-openai.api_key = "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"
+
 
 def index(request, *message):
-   
+
     messages.warning(request, message)
     num_artists = len(Artist.objects.all())
     festivals = Festival.objects.all()
@@ -28,17 +27,18 @@ def index(request, *message):
         return t
 
     env = Environment()
+
     env.filters['price'] = price
 
-    return render(request, 'public/index.html', {"festivals": festivals,
-                                                 "amount_fst": amount_fst,
-                                                 "amount_clients": amount_clients,
-                                                 "num_artists": num_artists,
-                                                 "descss": descss,
-                                                 "manager": manager,
-                                                
-                                                 })
+    context = {"festivals": festivals,
+               "amount_fst": amount_fst,
+               "amount_clients": amount_clients,
+               "num_artists": num_artists,
+               "descss": descss,
+               "manager": manager,
+               }
 
+    return render(request, 'public/index.html', context)
 
 
 def festival(request, festival_id):
@@ -50,38 +50,43 @@ def festival(request, festival_id):
     festival = get_object_or_404(Festival, pk=festival_id)
     festival_artists = Festival_Artist.objects.filter(festival=festival)
     artists = [fa.artist for fa in festival_artists]
-    num_tickets = festival.ticket_set.aggregate(total_tickets=Sum('amount'))['total_tickets']
-    num_sold_tickets = festival.ticket_set.aggregate(total_sold_tickets=Sum('amount_sold'))['total_sold_tickets']
+    num_tickets = festival.ticket_set.aggregate(
+        total_tickets=Sum('amount'))['total_tickets']
+    num_sold_tickets = festival.ticket_set.aggregate(
+        total_sold_tickets=Sum('amount_sold'))['total_sold_tickets']
     if num_tickets != None and num_sold_tickets != None:
         num_free_tickets = num_tickets - num_sold_tickets
     else:
         num_free_tickets = 0
     tickets = Ticket.objects.filter(festival=festival_id)
     names = (grou.name for grou in request.user.groups.all())
-    context = {'festival': festival, 
-               'num_tickets': num_tickets, 
-               "num_sold_tickets": num_sold_tickets, 
+    context = {'festival': festival,
+               'num_tickets': num_tickets,
+               "num_sold_tickets": num_sold_tickets,
                "num_free_tickets": num_free_tickets,
                "artists": artists,
-                "tickets": tickets,
-                "is_manager": is_manager,
-}
+               "tickets": tickets,
+               "is_manager": is_manager,
+               }
     return render(request, 'public/festival.html', context)
+
 
 def artist(request, artist_id):
     songs = Song.objects.filter(artist=artist_id)
     artist = get_object_or_404(Artist, pk=artist_id)
     ticket_types = ["Student", "Adult", "VIP"]
 
-    context = {'festival': festival, 
-                "artist": artist,
-                "ticket_types": ticket_types,
-                "songs": songs,
-    }
+    context = {'festival': festival,
+               "artist": artist,
+               "ticket_types": ticket_types,
+               "songs": songs,
+               }
     return render(request, 'public/artist.html', context)
+
 
 def test(request):
     return render(request, 'public/template/public_template.html')
+
 
 def buy(request, ticket_id):
     if User.is_authenticated:
